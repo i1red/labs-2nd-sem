b2c3b318ca01fece673dca48dacc0c4482e498d2
Ivan Krasnoshchok <46425174+i1red@users.noreply.github.com>
2019-09-15 16:44:42 +0300

    Add files via upload

--- graph-planarity/dinStruct.h
+++ graph-planarity/dinStruct.h
@@ -1,1 +1,462 @@
+#ifndef DINSTRUCT_H
+#define DINSTRUCT_H
 
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void ERROR(const char* log, int code)
+{
+    cout << "ERROR: " << log << endl;
+    system("pause");
+    exit(code);
+}
+
+template <typename T>
+class cList {
+    struct node {
+        T DATA;
+        node *NEXT;
+        node();
+        node(const node &other);
+        node(T, node *set_next = nullptr);
+        void copy(const node &other);
+        void set(T, node *set_next = nullptr);
+    };
+    node *HEAD;
+    node *TAIL;
+    int SIZE;
+public:
+    cList();
+    cList(const cList &);
+    size_t size();
+    void push(T);
+    void insert(T, const int);
+    void pop_index(const int);
+    bool pop_key(const T);
+    T& operator[](const int);
+	cList& operator=(const cList&);
+    ~cList();
+	friend ostream &operator << <T>(ostream&, const cList&);
+
+private:
+	void output(ostream&);
+};
+
+template <typename T>
+cList<T>::node::node()
+{
+    DATA = T(); NEXT = nullptr;
+}
+
+template <typename T>
+cList<T>::node::node(const node &other)
+{
+    DATA = other.DATA; NEXT = nullptr;
+}
+
+template <typename T>
+cList<T>::node::node(T set_data, node *set_next)
+{
+    DATA = set_data; NEXT = set_next;
+}
+
+template <typename T>
+void cList<T>::node::copy(const node &other)
+{
+    DATA = other.DATA; NEXT = other.NEXT;
+}
+
+template <typename T>
+void cList<T>::node::set(T set_data, node *set_next)
+{
+    DATA = set_data; NEXT = set_next;
+}
+
+template <typename T>
+cList<T>::cList()
+{
+    HEAD = TAIL = nullptr;
+    SIZE = 0;
+}
+
+template <typename T>
+cList<T>::cList(const cList &other)
+{
+	HEAD = TAIL = nullptr;
+	SIZE = 0;
+    node *tmp = other.HEAD;
+    while (tmp)
+    {
+        push(tmp->DATA);
+        tmp = tmp->NEXT;
+    }
+}
+
+template <typename T>
+size_t cList<T>::size()
+{
+    return SIZE;
+}
+
+template <typename T>
+void cList<T>::push(T push_data)
+{
+    if (!TAIL)
+        HEAD = TAIL = new node(push_data);
+    else
+    {
+		if (HEAD == TAIL) {
+			HEAD->NEXT = new node(push_data);
+			TAIL = HEAD->NEXT;
+		}
+		else {
+			TAIL->NEXT = new node(push_data);
+			TAIL = TAIL->NEXT;
+		}
+    }
+    SIZE++;
+}
+
+template <typename T>
+void cList<T>::insert(T insert_data, const int index)
+{
+    size_t smart_index;
+    if (index < 0)
+        smart_index = SIZE + index;
+    else
+        smart_index = index;
+    if (smart_index == SIZE)
+        push(insert_data);
+    else if (smart_index == 0)
+    {
+        HEAD = new node(insert_data, HEAD);
+        SIZE++;
+    }
+    else if (smart_index > 0 && smart_index < SIZE)
+    {
+        size_t counter = 0;
+        node *tmp = HEAD;
+        while (counter < smart_index - 1)
+        {
+            tmp = tmp->NEXT;
+            counter++;
+        }
+        tmp->NEXT = new node(insert_data, tmp->NEXT);
+        SIZE++;
+    }
+    else ERROR("Index is out of range", 1);
+
+}
+
+template <typename T>
+ostream &operator << (ostream &os, const cList<T> &sample) {
+	typedef cList<T>::node* nodeptr;
+	nodeptr tmp = sample.HEAD;
+	while (tmp) {
+	os << tmp->DATA;
+		if (tmp = tmp->NEXT)
+			os << ", ";
+	}
+	return os;
+}
+
+template <typename T>
+void cList<T>::pop_index(const int index)
+{
+    size_t smart_index;
+    if (index < 0)
+        smart_index = SIZE + index;
+    else
+        smart_index = index;
+    if (smart_index == 0 && SIZE == 1)
+    {
+        delete HEAD;
+        HEAD = TAIL = nullptr;
+        SIZE--;
+    }
+    else if (smart_index >= 0 && smart_index < SIZE)
+    {
+        size_t counter = 0;
+        node *tmp = HEAD, *ptr_to_del = nullptr;
+        while (counter < smart_index - 1)
+        {
+            tmp = tmp->NEXT;
+            counter++;
+        }
+        ptr_to_del = tmp->NEXT;
+        tmp->NEXT = ptr_to_del->NEXT;
+        if (ptr_to_del == TAIL)
+            TAIL = tmp;
+        delete ptr_to_del;
+        ptr_to_del = nullptr;
+        SIZE--;
+    }
+    else ERROR("Index is out of range", 1);
+}
+
+template <typename T>
+bool cList<T>::pop_key(const T key_data)
+{
+    node *ptr_to_del = HEAD, *tmp = nullptr;
+    while (ptr_to_del && ptr_to_del->DATA != key_data)
+    {
+        tmp = ptr_to_del;
+        ptr_to_del = ptr_to_del->NEXT;
+    }
+    if (!ptr_to_del)
+        return false;
+    else
+    {
+        if (tmp)
+        {
+            tmp->NEXT = ptr_to_del->NEXT;
+            if (ptr_to_del == TAIL)
+                TAIL = tmp;
+            delete ptr_to_del;
+            ptr_to_del = nullptr;
+        }
+        else
+        {
+            delete HEAD;
+            HEAD = TAIL = nullptr;
+        }
+        SIZE--;
+        return true;
+    }
+}
+
+template <typename T>
+T& cList<T>::operator[](const int index)
+{
+    size_t smart_index;
+    if (index < 0)
+        smart_index = SIZE + index;
+    else
+        smart_index = index;
+    if (smart_index >= 0 && smart_index < SIZE)
+    {
+        size_t counter = 0;
+        node *tmp = HEAD;
+        while (counter < smart_index)
+        {
+            tmp = tmp->NEXT;
+            counter++;
+        }
+        return tmp->DATA;
+    }
+    else ERROR("Index is out of range", 1);
+}
+
+template <typename T>
+cList<T>& cList<T>::operator=(const cList<T> &other) {
+	node *tmp = HEAD, *tmpother = other.HEAD;
+	if (SIZE >= other.SIZE) {
+		node *tmplast = nullptr;
+		while (tmpother) {
+			tmp->DATA = tmpother->DATA;
+			tmplast = tmp;
+			tmp = tmp->NEXT, tmpother = tmpother->NEXT;
+		}
+		if (tmplast)
+			tmplast->NEXT = nullptr;
+		while (tmp) {
+			node *tmp2 = tmp;
+			tmp = tmp->NEXT;
+			delete tmp2;
+		}
+	}
+	else {
+		while (tmp) {
+			tmp->DATA = tmpother->DATA;
+			tmp = tmp->NEXT, tmpother = tmpother->NEXT;
+		}
+		while (tmpother) {
+			push(tmpother->DATA);
+			tmpother = tmpother->NEXT;
+		}
+	}
+	SIZE = other.SIZE;
+	return *this;
+}
+
+template <typename T>
+cList<T>::~cList() {
+    node *tmp = HEAD;
+    while (tmp) {
+        node *tmp2 = tmp;
+        tmp = tmp->NEXT;
+        delete tmp2;
+    }
+
+}
+
+template <typename T>
+class cQueue {
+    struct queue_obj {
+        T DATA;
+        queue_obj *NEXT;
+    };
+    queue_obj *HEAD;
+    queue_obj *TAIL;
+public:
+    cQueue()
+    {
+        HEAD = TAIL = nullptr;
+    }
+    cQueue(const cQueue& other)
+    {
+        HEAD = TAIL = nullptr;
+        queue_obj *tmp = other.HEAD;
+        while (tmp)
+        {
+            push(tmp->DATA);
+            tmp = tmp->NEXT;
+        }
+    }
+    void push(T data_obj)
+    {
+        queue_obj *tmp = new queue_obj;
+        tmp->DATA = data_obj;
+        tmp->NEXT = nullptr;
+        if (!HEAD)
+            HEAD = TAIL = tmp;
+        else
+        {
+            TAIL->NEXT = tmp;
+            TAIL = TAIL->NEXT;
+        }
+    }
+    bool is_empty()
+    {
+        if (!HEAD)
+            return true;
+        else
+            return false;
+    }
+    T pop()
+    {
+        if (!HEAD)
+        {
+            cout << "ERROR: Can't pop <empty queue>" << endl;
+            system("pause");
+            exit(1);
+        }
+        else
+        {
+            queue_obj *tmp = HEAD;
+            T res = HEAD->DATA;
+            HEAD = HEAD->NEXT;
+            delete tmp;
+            return res;
+        }
+    }
+    T peek()
+    {
+        if (!HEAD)
+        {
+            cout << "ERROR: Can't peek <empty queue>" << endl;
+            system("pause");
+            exit(1);
+        }
+        else
+            return HEAD->DATA;
+    }
+    ~cQueue()
+    {
+        queue_obj *tmp = HEAD;
+        while (tmp)
+        {
+            queue_obj *tmp2 = tmp;
+            tmp = tmp->NEXT;
+            delete tmp2;
+        }
+    }
+};
+
+template <typename T>
+struct cStack {
+private:
+    struct cell {
+        T DATA;
+        cell *NEXT;
+    };
+    cell *HEAD;
+    int SIZE;
+public:
+    cStack()
+    {
+        HEAD = nullptr;
+        SIZE = 0;
+    }
+    cStack(const cStack &other)
+    {
+        HEAD = nullptr;
+        SIZE = 0;
+        cell *tmp = other.HEAD;
+        while (tmp)
+        {
+            push(tmp->DATA);
+            tmp = tmp->NEXT;
+        }
+    }
+    int size()
+    {
+        return SIZE;
+    }
+    bool is_empty()
+    {
+        if (!HEAD)
+            return true;
+        else
+            return false;
+    }
+    void push(T object)
+    {
+        cell *tmp = new cell;
+        tmp->DATA = object;
+        tmp->NEXT = HEAD;
+        HEAD = tmp;
+        SIZE++;
+    }
+    T pop()
+    {
+        if (!HEAD)
+        {
+            cout << "ERROR: Can't pop element <stack is empty> " << endl;
+            system("pause");
+            exit(1);
+        }
+        else
+        {
+            T cellData = HEAD->DATA;
+            cell *tmp = HEAD;
+            HEAD = HEAD->NEXT;
+            delete tmp;
+            SIZE--;
+            return cellData;
+        }
+    }
+    T& peek()
+    {
+        if (!HEAD)
+        {
+            cout << "ERROR: Can't peek element <stack is empty> " << endl;
+            system("pause");
+            exit(1);
+        }
+        else
+            return HEAD->DATA;
+    }
+    ~cStack()
+    {
+        cell *tmp = HEAD;
+        while (tmp)
+        {
+            cell *tmp2 = tmp;
+            tmp = tmp->NEXT;
+            delete tmp2;
+        }
+    }
+};
+#endif 
---
+++ graph-planarity/main.cpp
@@ -0,0 +1,333 @@
+#include <iostream>
+#include "C:\include\dinStruct.h"
+using namespace std;
+
+struct subgraph;
+
+class graph {
+	cList<int> *vrtx; //масив списків, кожен з яких являє собою набір суміжних вершин для вершини масиву i
+	size_t vn;//кількість вершин
+	int *e3, *e4; //масиви, що зберігають номери вершин степеня 3+ та 4+ відповідно
+	size_t v3, v4; //кількість вершин степеня 3+ та 4+ відповідно
+public:
+	graph(int*, size_t);
+	void print();
+	void showE3();
+	void showE4();
+	bool planar();
+	~graph();
+private:
+	bool fK5(int, int, int, int, int); //ф-ї, що перевіряють чи можна знайти граф гомеоморфний К5 / К33 для заданих вершин
+	bool fK33(int, int, int, int, int, int); //ці ф-ї будують об'єкт subgraph(зокрема для К33 враховуються всі варіанти обрання часток), який передають у ф-ю sub
+	void sub(bool&, subgraph);//ф-я опрацювує всі можливі варіанти підграфа(враховуються усі можливі шляхи, що можна замінити на ребро, для кожного такого ребра отримуємо новий subgraph)
+	void findpath(bool&, stack<subgraph>&, subgraph, int, cQueue<int>);//ф-я знаходить усі можливі шляхи від однієї вершини до іншої
+};
+
+
+int main() {
+
+	int mtrx[] =
+	{ 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0,
+1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1,
+1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1,
+0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0
+	};
+	graph sample(mtrx, 17);
+	sample.print();
+	cout << "degu3+: ";
+	sample.showE3();
+	cout << "degu4+: ";
+	sample.showE4();
+
+	cout << "\n==================================\n" << endl;
+	bool check = sample.planar();
+	cout << "RESULT: Graph is ";
+	if (!check)
+		cout << "NOT ";
+	cout << "planar" << endl;
+	cout << "\n==================================" << endl;
+	system("pause");
+	return 0;
+}
+
+//================subgraph=================
+
+struct subgraph {
+	int v[6];//вершини підграфа, що розглядається
+	int st, dst;//вершини між якими шукаємо шлях
+	bool *vstd;//відмітки для вершин, що були переглянуті у рамках пошуку одного шляху(запобігання циклічності)
+	bool *lckd;//вершини, що належать певному із шляхів графа, що були знайдені(заборона повторного викристання для пошуку нових шляхів)
+	size_t cycle_count;//використовуєтся у функції block(рахуємо кількість разів використання вершини v[st])
+	size_t vs;//кількість вершин графа, з якого виділено підграф
+	size_t subvs;//кількість вершин підграфа(для того, щоб відрізняти чи шукаємо К5, чи К33)
+	subgraph(size_t vnum, int v0, int v1, int v2, int v3, int v4, int v5 = -1) {
+		v[0] = v0, v[1] = v1, v[2] = v2, v[3] = v3, v[4] = v4, v[5] = v5;
+
+		if (v5 == -1)
+			subvs = 5, st = 0, dst = 1;
+		else
+			subvs = 6, st = 0, dst = 3;
+
+		vstd = new bool[vs = vnum]();
+		lckd = new bool[vs]();
+		cycle_count = 0;
+	}
+	subgraph(const subgraph &other) {
+		vs = other.vs;
+		subvs = other.subvs;
+		st = other.st;
+		dst = other.dst;
+		cycle_count = other.cycle_count;
+		vstd = new bool[vs];
+		lckd = new bool[vs];
+		for (size_t i = 0; i < vs; i++)
+		{
+			vstd[i] = other.vstd[i];
+			lckd[i] = other.lckd[i];
+		}
+		for (size_t i = 0; i < 6; i++)
+			v[i] = other.v[i];
+	}
+	int start() {
+		return v[st];
+	}
+	int dest() {
+		return v[dst];
+	}
+	//функція перевіяє чи належить вершина підграфу
+	bool block(int curr) {
+		if (curr == v[st])
+		{
+			if (cycle_count)
+				return true;
+			else
+				cycle_count++;
+		}
+		else
+		{
+			for (size_t i = 0; i < subvs; i++)
+			{
+				if (i == st || i == dst)
+					continue;
+				if (curr == v[i])
+					return true;
+			}
+		}
+		return false;
+	}
+	void nextEdge() {
+		if (subvs == 5)
+		{
+			if (++dst == 5)
+				++st, dst = st + 1;
+		}
+		else
+		{
+			if (++dst == 6)
+				++st, dst = 3;
+		}
+		cycle_count = 0;
+	}
+	~subgraph() {
+		delete[] vstd;
+		delete[] lckd;
+	}
+
+	void RESET_VISITED() {
+		for (size_t i = 0; i < vs; i++)
+			vstd[i] = false;
+	}
+};
+
+//*****************************************
+
+//=================graph===================
+
+graph::graph(int *matrix, size_t msize) {
+	vrtx = new cList<int>[vn = msize];
+	cStack<int> edges3, edges4;
+	for (size_t i = 0; i < msize; i++)
+		for (size_t j = 0; j < msize; j++)
+			if (matrix[j + i * msize] == 1)
+			{
+				vrtx[i].push(j);
+				int sz = vrtx[i].size();
+				if (sz == 3)
+					edges3.push(i);
+				else if (sz == 4)
+					edges4.push(i);
+			}
+	if (v3 = edges3.size())
+	{
+		e3 = new int[v3];
+		for (size_t i = v3 - 1; !edges3.is_empty(); i--)
+			e3[i] = edges3.pop();
+	}
+	else
+		e3 = nullptr;
+	if (v4 = edges4.size())
+	{
+		e4 = new int[v4];
+		for (size_t i = v4 - 1; !edges4.is_empty(); i--)
+			e4[i] = edges4.pop();
+	}
+	else
+		e4 = nullptr;
+}
+
+void graph::print() {
+	for (size_t i = 0; i < vn; i++)
+	{
+		cout << i << " : ";
+		cout << vrtx[i] << endl;
+	}
+}
+
+void graph::showE3() {
+	if (!e3)
+	{
+		cout << "0 vertices deg>=3" << endl;
+		return;
+	}
+	for (size_t i = 0; i < v3 - 1; i++)
+		cout << e3[i] << ", ";
+	cout << e3[v3 - 1] << endl;
+}
+
+void graph::showE4() {
+	if (!e4)
+	{
+		cout << "0 vertices deg>=4" << endl;
+		return;
+	}
+	for (size_t i = 0; i < v4 - 1; i++)
+		cout << e4[i] << ", ";
+	cout << e4[v4 - 1] << endl;
+}
+
+bool graph::planar() {
+	if (v4 >= 5)
+	{
+		for (size_t i = 0; i < v4; i++)
+			for (size_t j = i + 1; j < v4; j++)
+				for (size_t m = j + 1; m < v4; m++)
+					for (size_t n = m + 1; n < v4; n++)
+						for (size_t p = n + 1; p < v4; p++)
+							if (fK5(e4[i], e4[j], e4[m], e4[n], e4[p])) {
+								cout << "Found subgraph K5" << endl;
+								cout << e4[i] << ", " << e4[j] << ", " << e4[m] << ", " << e4[n] << ", " << e4[p] << endl;
+								return false;
+							}
+	}
+	if (v3 >= 6)
+	{
+		for (size_t i = 0; i < v3; i++)
+			for (size_t j = i + 1; j < v3; j++)
+				for (size_t m = j + 1; m < v3; m++)
+					for (size_t n = m + 1; n < v3; n++)
+						for (size_t p = n + 1; p < v3; p++)
+							for (size_t q = p + 1; q < v3; q++)
+								if (fK33(e3[i], e3[j], e3[m], e3[n], e3[p], e3[q])) 
+									return false;
+	}
+	return true;
+}
+
+bool graph::fK5(int v0, int v1, int v2, int v3, int v4)
+{
+	bool res = false;
+	sub(res, subgraph(vn, v0, v1, v2, v3, v4));
+	return res;
+}
+
+bool graph::fK33(int v0, int v1, int v2, int v3, int v4, int v5) {
+	bool res = false;
+	int v[] = { v0, v1, v2, v3, v4, v5 };
+	for (size_t i = 1; i < 6; i++)
+		for (size_t j = i + 1; j < 6; j++)
+		{
+			int tmp[6];
+			for (size_t k = 0; k < 6; k++)
+				tmp[k] = v[k];
+			swap(tmp[1], tmp[i]);
+			swap(tmp[2], tmp[j]);
+			sub(res, subgraph(vn, tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5]));
+			if (res)
+			{
+				cout << "Found subgraph K33" << endl;
+				cout << tmp[0] << ", " << tmp[1] << ", " << tmp[2] << ", " << tmp[3] << ", " << tmp[4] << ", " << tmp[5] << endl;
+				return true;
+			}
+		}
+	return false;
+}
+
+
+void graph::sub(bool &res, subgraph grph) {
+	if (grph.subvs == 5 && grph.st == 4)
+		res = true;
+	else if (grph.subvs == 6 && grph.st == 3)
+		res = true;
+	else
+	{
+		stack<subgraph> cont;
+		bool flag = false;
+		findpath(flag, cont, grph, grph.start(), *new cQueue<int>);
+		if (!flag)
+			return;
+		else
+		{
+			while (cont.size())
+			{
+				sub(res, cont.top());
+				cont.pop();
+			}
+		}
+	}
+}
+
+void graph::findpath(bool &res, stack<subgraph> &container, subgraph grph, int curr, cQueue<int> path) {
+	if (curr == grph.dest())
+	{
+		res = true;
+		while (!path.is_empty())
+			grph.lckd[path.pop()] = true;
+		grph.RESET_VISITED();
+		grph.nextEdge();
+		container.push(grph);
+	}
+	else if (grph.vstd[curr] || grph.lckd[curr] || grph.block(curr))
+		return;
+	else
+	{
+		if (curr != grph.start())
+		{
+			grph.vstd[curr] = true;
+			path.push(curr);
+		}
+		for (size_t i = 0; i < vrtx[curr].size(); i++)
+			findpath(res, container, grph, vrtx[curr][i], path);
+	}
+}
+
+
+graph::~graph() {
+	delete[] vrtx;
+	delete[] e3;
+	delete[] e4;
+}
+
+//******************************************
